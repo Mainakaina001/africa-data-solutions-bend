6c625be558aa9374d8cf9b5e3d694dde32b10b86
Suleiman Ibrahim Inuwa <suleimanibrahimsaban@gmail.com>
2026-08-17 22:44:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# africa-data-solutions-bend
---
+++ src/main/java/afds/africadatasolution/AfricaDataSolutionApplication.java
@@ -0,0 +1,13 @@
+package afds.africadatasolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfricaDataSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfricaDataSolutionApplication.class, args);
+	}
+
+}
